a62e65d36054ac20b0d5ebed8f9e9fc16812c08f
Shaleni <skovach@smu.edu>
2017-12-02 13:21:07 -0600

    S1 Counting Cards (all options), fixed bugs
    Found bugs in assignemtn of bets to totalBet and AmtLeft. Fixed in winner, winnerVerbose, and S1

--- winner.R
+++ winner.R
@@ -2,12 +2,15 @@
 # Takes the player's hand, the dealer's hand, split hand (optional), if using doubling down logic (optional)
 # Returns a list containing the dealer's hand, and boolean if the dealer is standing
 
-checkForWinner<-function(playerCards,dealerCards,splitPlayerCards, doublingdown){
+checkForWinner<-function(playerCards,dealerCards,splitPlayerCards, doublingdown, tc){
   #initalize values
   numBlackJack <- numOtherWin <- 0
   numTie <- numLoss<- numBust <-0
   amtLeft <- totalBet <-0
   
+  #normal bet is $1/game
+  betAmt <- 1
+
   #set flags
   pBust <- sum(playerCards)>21
   dBust <- sum(dealerCards)>21
@@ -18,66 +21,58 @@ checkForWinner<-function(playerCards,dealerCards,splitPlayerCards, doublingdown)
   	doublingdown=FALSE
   }
   
-  doubleResult = F
-  #check if need to award double cost or nt or not
-  if(doublingdown){
-  	 if(playerCards[1]+playerCards[2]==10 || playerCards[1]+playerCards[2]==11){
-  	doubleResult=T
-  	}
+  #see if bet needs to change b/c counting cards
+  #anything negative will be normal bets
+  if(missing(tc)){
+    tc <- -1
   }
  
   #if it was not a split hand, run as normal
   if(missing(splitPlayerCards)){
+    #if tc is non-negative, betAmt will be $(tc+2)
+    if(tc>-1){
+      betAmt <- tc+2
+    }
+
+    #check if betting double ($2)
+    if(doublingdown){
+       if(playerCards[1]+playerCards[2]==10 || playerCards[1]+playerCards[2]==11){
+        betAmt <- betAmt * 2
+      }
+    }
+
     #Determine winner and assign money
     #if player is bust, loses money
     #if doubling down lose/win twice as much
     if(pBust){
-      amtLeft <- amtLeft - 1
+      amtLeft <- amtLeft - betAmt
       numBust <- numBust +1
-      if(doubleResult){
-      	amtLeft <- amtLeft - 1
-      }
     } else if (dBlackjack){
       if (!pBlackjack){
-        amtLeft <- amtLeft - 1
+        amtLeft <- amtLeft - betAmt
         numLoss <- numLoss +1
-        if(doubleResult){
-        	amtLeft <- amtLeft - 1
-        }
       } else{
         #if both get blackjack, no money exchanged
         numTie <- numTie + 1
       }
     } else if (pBlackjack){
       #player wins
-      amtLeft <- amtLeft + 2.5
-      if(doubleResult){
-      	amtLeft <- amtLeft + 2.5
-      }
+      amtLeft <- amtLeft + (1.5*betAmt)
       numBlackJack <- numBlackJack + 1
     } else if (dBust){
       #player wins
-      amtLeft <- amtLeft + 2
-      if(doubleResult){
-      	numOtherWin <- numOtherWin + 1
-      }
+      amtLeft <- amtLeft + betAmt
       numOtherWin <- numOtherWin + 1
     } else if (sum(playerCards)>sum(dealerCards)){
       #player wins
-      amtLeft <- amtLeft + 2
-      if(doubleResult){
-      	numOtherWin <- numOtherWin + 1
-      }
+      amtLeft <- amtLeft + betAmt
       numOtherWin <- numOtherWin + 1
     } else if (sum(playerCards)==sum(dealerCards)){
       #if both get same amount, no money exchanged
       numTie <- numTie + 1
     }else{
       #player loses
-      amtLeft <- amtLeft - 1
-      if(doubleResult){
-      	amtLeft <- amtLeft - 1
-      }
+      amtLeft <- amtLeft - betAmt
       numLoss <- numLoss + 1
     }
 
@@ -88,23 +83,23 @@ checkForWinner<-function(playerCards,dealerCards,splitPlayerCards, doublingdown)
     #rather than the normal 1 1/2 times
     #otherwise, call checkForWinner with each hand and return the combined result
     if(playerCards[1]==11 && sum(playerCards)==21){
-      amtLeft <- amtLeft + 2
+      amtLeft <- amtLeft + betAmt
       numBlackJack <- numBlackJack + 1
     } else {
     	if(doublingdown){
-    		checkForWinner(playerCards,dealerCards, doublingdown=TRUE)
+    		checkForWinner(playerCards,dealerCards, doublingdown=TRUE, tc=tc)
     		} else{
-    			checkForWinner(playerCards,dealerCards)
+    			checkForWinner(playerCards,dealerCards, tc=tc)
     		}   
     }
     if(splitPlayerCards[1]==11 && sum(splitPlayerCards)==21){
-      amtLeft <- amtLeft + 2
+      amtLeft <- amtLeft + betAmt
       numBlackJack <- numBlackJack + 1
     } else {
     	if(doublingdown){
-    		checkForWinner(splitPlayerCards,dealerCards, doublingdown=TRUE)
+    		checkForWinner(splitPlayerCards,dealerCards, doublingdown=TRUE, tc=tc)
     	} else{
-    		checkForWinner(splitPlayerCards,dealerCards)
+    		checkForWinner(splitPlayerCards,dealerCards, tc=tc)
     	}    
     }
     return(cbind(numBlackJack,numOtherWin,numTie,numLoss,numBust,totalBet,amtLeft))
